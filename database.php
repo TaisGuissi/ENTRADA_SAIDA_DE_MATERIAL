3292d424bd95617bf5070790162c8760fb3467ce
Tais Guissi <71243004+TaisGuissi@users.noreply.github.com>
2022-06-04 15:18:02 -0300

    Create database.php
    Acesso ao banco de dados

---
+++ database.php
@@ -0,0 +1,14 @@
+<?php
+
+//DECLARANDO UMA CONSTANTE PARA O ACESSO DEO BANCO DE DADOS
+
+define("SERVER_LOCAL","localhost");
+define("SERVER_USER","root");
+define("SERVER_PASSWORD","");
+define("DATABASE_NAME","si_ads_2period");
+
+$conexao = mysqli_connect(SERVER_LOCAL,SERVER_USER,SERVER_PASSWORD,DATABASE_NAME);
+
+
+
+?>
