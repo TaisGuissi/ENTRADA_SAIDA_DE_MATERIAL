19bd46b26307370771274719bf238210a16a875f
Tais Guissi <71243004+TaisGuissi@users.noreply.github.com>
2022-06-04 17:07:19 -0300

    Update formentrada.php

---
+++ formentrada.php
@@ -0,0 +1,68 @@
+<?php require_once "header.php"?>
+
+<h1>Sistema de Gerenciamento de Escolar</h1>
+
+
+<fieldset>
+
+<!-- FORMUÁRIO DE CADASTRO DE ALUNOS -->
+<form action="entrada.php" method="POST">
+
+    <div class="form-row">
+    
+    </div>
+
+    <legend>Entrada de MP</legend><br><br>
+
+    <label for="data_entrada">Data: </label>
+    <input type="text" name="data_entrada" id="data_entrada"><br><br>
+
+    <label for="student_email">Hora:</label>
+    <input type="hora_entrada" name="hora_entrada" id="hora_entrada"><br><br>
+
+    <label for="hora_saida">Saída:</label>
+    <input type="text" name="hora_saida" id="hora_saida"><br><br>
+
+    <label for="cod_prod">Código: </label>
+    <input type="text" name="cod_prod" id="cod_prod"><br><br>
+
+    <label for="desc_prod">Descrição:</label>
+    <input type="text" name="desc_prod" id="desc_prod"><br><br>
+
+    <label for="unidade_prod">Unidade:</label>
+    <input type="text" name="unidade_prod" id="unidade_prod"><br><br>
+
+    <label for="qtd_prod">Quantidade:</label>
+    <input type="text" name="qtd_prod" id="qtd_prod"><br><br>
+
+    <label for="nf_prod">Nota Fiscal:</label>
+    <input type="text" name="nf_prod" id="nf_prod"><br><br>
+    
+
+
+
+
+    <input type="submit" value="Cadastrar Aluno">
+
+
+</form>
+
+
+</fieldset>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php require_once "footer.php"?>
